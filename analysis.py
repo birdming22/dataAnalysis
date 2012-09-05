4795551ec79eeed72f5d6f263a0136bb8aa9ec5f
Birdming Lai <birdming@gmail.com>
2012-09-05 10:03:52 -0400

    refactor: coding style

--- analysis.py
+++ analysis.py
@@ -6,22 +6,21 @@ import datalink
 IN_FILE = "71.dat"
 
 def main():
-	dl = datalink.Datalink()
+    dl = datalink.Datalink()
     # read file
-	with open(IN_FILE, 'rb') as f:
-		c = f.read(1)
-		dl.processMessage(ord(c))
-		while c:
-			#print ord(c)
-			c = f.read(1)
-			try:
-				data = dl.processMessage(ord(c))
-				if data is not None:
-					if len(data) != 16:
-						print 'data:', data
-
-			except:
-				pass
+    with open(IN_FILE, 'rb') as f:
+        c = f.read(1)
+        dl.processMessage(ord(c))
+        while c:
+            #print ord(c)
+            c = f.read(1)
+            try:
+                data = dl.processMessage(ord(c))
+                if data is not None:
+                    if len(data) != 16:
+                        print 'data:', data
+            except:
+                pass
 
 
 if __name__ == '__main__':
